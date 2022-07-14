e68322b2f94598a4d794b504f5d808753634e43e
alelozada <ale.lozadag@gmail.com>
2022-07-14 11:39:22 -0500

    Update information to June 2022

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,12 +40,12 @@ $(function() {
         if(bodyScroll > 100){
 
             navbar.addClass("nav-scroll");
-            logo.attr('src', 'img/logo-dark.png');
+            logo.attr('src', 'assets/img/alelozada-logo.png');
 
         }else{
 
             navbar.removeClass("nav-scroll");
-            logo.attr('src', 'img/logo-light.png');
+            logo.attr('src', 'assets/img/alelozada-logo.png');
         }
     });
 
